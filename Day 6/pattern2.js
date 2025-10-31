df5cdb3213d611dc369bd41deb5b631aad0fb27d
Vikas <vikas.vp706@gmail.com>
2025-10-31 11:53:11 +0530

    Day 6-2

---
+++ Day 6/pattern2.js
@@ -0,0 +1,22 @@
+/*
+  Print the given pattern
+
+  * 
+  * * 
+  * * * 
+  * * * *
+  * * * * *
+  * * * * *
+  * * * * 
+  * * *
+  * *
+  * 
+*/
+
+for (i = 0; i < 10; i++) {
+  let row = ' ';
+  for (j = i; j < 5; j++) {
+    row += '* ';
+  }
+  console.log(row);
+}
